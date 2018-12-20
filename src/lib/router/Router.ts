6130f3c40fea41a471c787f9fb7b825b27d3515c
ranyunlong <549510622@qq.com>
2018-12-20 20:29:01 +0800

    update

--- src/lib/router/Router.ts
+++ src/lib/router/Router.ts
@@ -14,6 +14,7 @@ import { Context } from '../core/Context';
 import { RegExpOptions } from 'path-to-regexp'
 import { MetadataEnums, RequestEnums } from '../enums';
 import EasyBootServlet from '../core/EasyBootServlet';
+import { HttpException } from '../core';
 
 export class Router {
     public routes: Route[] = []
@@ -37,34 +38,60 @@ export class Router {
     public async handleResponse(context: Context) {
         const stack = this.matchRoute(context.path, context.method as RequestEnums.METHOD)
         for (let layer of stack) {
-            this.application.once('err', (error) => {
+            const handlerException = (error: HttpException) => {
                 if (layer.exceptionCapture) {
                     this.application.exception(context, new layer.exceptionCapture(error))
                 }
                 if (layer.exception) {
                     this.application.exception(context, layer.exception)
                 }
-            })
+            }
+
+            // Add exception event listener
+            this.application.once('err', handlerException)
+
+            // Check body is writeble
             if (context.response.body) return;
+
+            // Parse request path params metadata
             await layer.parseParamMetadata(context)
+
+            // Parse request query string metadata
             await layer.parseQueryMetadata(context)
+
+            // Parse request body metadata
             await layer.parseBodyMetadata(this.application.bodyParserService, context)
+
+            // Inject Request Object
             await layer.parseRequestMetadata(context)
+
+            // Inject Response Object
             await layer.parseResponseMetadata(context)
+
             const { Controller, propertyKey, Mod } = layer
+            // Controllr inject service
             let metadata = Reflect.getMetadata(MetadataEnums.Base.PARAMTYPES, Controller) || []
             if (Array.isArray(metadata)) {
                 metadata = metadata.map((Service) => {
                     return this.application.metadataManager.queryProviders(Mod, Service)
                 })
             }
+
+            // Handler response
             const data = await new Controller(...metadata)[propertyKey](...layer.handleMetadatas)
             if (data) {
                 context.body = data
+
+                // Set response status code
                 if (layer.statusCode) context.response.status = layer.statusCode
+                // Set response status message
                 if (layer.statusMessage) context.response.message = layer.statusMessage
+                // Set response content type
                 if (layer.contentType) context.response.type = layer.contentType
             }
+
+            // If not exception, cancel exception event listener
+            this.application.off('err', handlerException)
         }
     }
 }
