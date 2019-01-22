4b968f8cbc19441077b058eda2410b6a668aa9a6
ranyunlong <549510622@qq.com>
2019-01-22 21:59:46 +0800

    updaste

---
+++ src/application/index.ts
@@ -0,0 +1,8 @@
+import { Servlet, Configuration, ServletConfiguration } from '@easyboot/core'
+
+@Configuration(ServletConfiguration)
+export class Application extends Servlet {
+    public bootstrap() {
+        
+    }
+}
