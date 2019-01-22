a3fd347c1b487937253ea85364ec6fc7163f65b2
ranyunlong <549510622@qq.com>
2019-01-22 23:49:40 +0800

    update

--- src/application/configs/AppConfig.ts
+++ src/application/configs/AppConfig.ts
@@ -3,5 +3,5 @@ import { resolve } from 'path'
 
 export class AppConfig extends ServletConfiguration {
     public port: number = 5000
-    public staticDir: string = resolve('public')
+    public staticDir: string = resolve('static')
 }
